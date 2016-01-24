206bcd83bbe79fdaa91bde78893f6cb2f93c9970
dcubides <davidcubides.90@gmail.com>
2016-01-24 14:18:39 -0500

    control

--- CONTROLM/aplicacion/application/views/informe_movimientos.php
+++ CONTROLM/aplicacion/application/views/informe_movimientos.php
@@ -1,21 +1,168 @@
 
 
-<?php 
-    foreach($css_files as $file): ?>
-    	<link type="text/css" rel="stylesheet" href="<?php echo $file; ?>" />
-    <?php endforeach; ?>
-    <?php foreach($js_files as $file): ?>
-    	<script src="<?php echo $file; ?>"></script>
-    <?php endforeach; ?>
-    
-   
-    	
-<!--Body content-->
-       
-            <div class="contentwrapper"><!--Content wrapper-->
-                <!-- Build page from here: -->
-                <div class="row-fluid">
-					<?php echo $output; ?>
-                </div><!-- End .row-fluid -->
-                <!--End page -->
-                
+<div class="container">
+    <h1>Informe de Movimientos</h1>
+ 
+    <h3>Datos</h3>
+    <br />
+    <button class="btn btn-success" onclick="add_person()"><i class="glyphicon glyphicon-plus"></i> Add Person</button>
+    <br />
+    <br />
+    <table id="table" class="table table-striped table-bordered" cellspacing="0" width="100%">
+      <thead>
+        <tr>
+          <th>First Name</th>
+          <th>Last Name</th>
+          <th>Gender</th>
+          <th>Address</th>
+          <th>Date of Birth</th>
+          <th style="width:125px;">Action</th>
+        </tr>
+      </thead>
+      <tbody>
+      </tbody>
+ 
+      <tfoot>
+        <tr>
+          <th>First Name</th>
+          <th>Last Name</th>
+          <th>Gender</th>
+          <th>Address</th>
+          <th>Date of Birth</th>
+          <th>Action</th>
+        </tr>
+      </tfoot>
+    </table>
+  </div>
+
+
+
+   <script type="text/javascript">
+ 
+    var save_method; //for save method string
+    var table;
+    $(document).ready(function() {
+      table = $('#table').DataTable({
+ 
+        "processing": true, //Feature control the processing indicator.
+        "serverSide": true, //Feature control DataTables' server-side processing mode.
+ 
+        // Load data for the table's content from an Ajax source
+        "ajax": {
+            "url": "<?php echo site_url('person/ajax_list')?>",
+            "type": "POST"
+        },
+ 
+        //Set column definition initialisation properties.
+        "columnDefs": [
+        {
+          "targets": [ -1 ], //last column
+          "orderable": false, //set not orderable
+        },
+        ],
+ 
+      });
+    });
+ 
+    function add_person()
+    {
+      save_method = 'add';
+      $('#form')[0].reset(); // reset form on modals
+      $('#modal_form').modal('show'); // show bootstrap modal
+      $('.modal-title').text('Add Person'); // Set Title to Bootstrap modal title
+    }
+ 
+    function edit_person(id)
+    {
+      save_method = 'update';
+      $('#form')[0].reset(); // reset form on modals
+ 
+      //Ajax Load data from ajax
+      $.ajax({
+        url : "<?php echo site_url('person/ajax_edit/')?>/" + id,
+        type: "GET",
+        dataType: "JSON",
+        success: function(data)
+        {
+ 
+            $('[name="id"]').val(data.id);
+            $('[name="firstName"]').val(data.firstName);
+            $('[name="lastName"]').val(data.lastName);
+            $('[name="gender"]').val(data.gender);
+            $('[name="address"]').val(data.address);
+            $('[name="dob"]').val(data.dob);
+ 
+            $('#modal_form').modal('show'); // show bootstrap modal when complete loaded
+            $('.modal-title').text('Edit Person'); // Set title to Bootstrap modal title
+ 
+        },
+        error: function (jqXHR, textStatus, errorThrown)
+        {
+            alert('Error get data from ajax');
+        }
+    });
+    }
+ 
+    function reload_table()
+    {
+      table.ajax.reload(null,false); //reload datatable ajax
+    }
+ 
+    function save()
+    {
+      var url;
+      if(save_method == 'add')
+      {
+          url = "<?php echo site_url('person/ajax_add')?>";
+      }
+      else
+      {
+        url = "<?php echo site_url('person/ajax_update')?>";
+      }
+ 
+       // ajax adding data to database
+          $.ajax({
+            url : url,
+            type: "POST",
+            data: $('#form').serialize(),
+            dataType: "JSON",
+            success: function(data)
+            {
+               //if success close modal and reload ajax table
+               $('#modal_form').modal('hide');
+               reload_table();
+            },
+            error: function (jqXHR, textStatus, errorThrown)
+            {
+                alert('Error adding / update data');
+            }
+        });
+    }
+ 
+    function delete_person(id)
+    {
+      if(confirm('Are you sure delete this data?'))
+      {
+        // ajax delete data to database
+          $.ajax({
+            url : "<?php echo site_url('person/ajax_delete')?>/"+id,
+            type: "POST",
+            dataType: "JSON",
+            success: function(data)
+            {
+               //if success reload ajax table
+               $('#modal_form').modal('hide');
+               reload_table();
+            },
+            error: function (jqXHR, textStatus, errorThrown)
+            {
+                alert('Error adding / update data');
+            }
+        });
+ 
+      }
+    }
+ 
+  </script>
+ 
+ 
